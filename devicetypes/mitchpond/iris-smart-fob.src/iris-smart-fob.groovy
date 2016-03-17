d35c47cc54efff2fab4f8d2ab77277910a931e76
Mitch Pond <mitchp@gmail.com>
2016-03-16 20:00:39 -0500

    Added presence detection, streamlined some of the button press/hold code.

--- devicetypes/mitchpond/iris-smart-fob.src/iris-smart-fob.groovy
+++ devicetypes/mitchpond/iris-smart-fob.src/iris-smart-fob.groovy
@@ -2,6 +2,7 @@
  *  Iris Smart Fob
  *
  *  Copyright 2015 Mitch Pond
+ *	Presence code adapted from SmartThings Arrival Sensor HA device type
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -18,77 +19,92 @@ metadata {
 		capability "Battery"
 		capability "Button"
         capability "Configuration"
+		capability "Presence Sensor"
 		capability "Sensor"
 
-		command "test"
-        
-        attribute "lastPress", "string"
+		command "testCmd"
         
 		fingerprint endpointId: "01", profileId: "0104", inClusters: "0000,0001,0003,0007,0020,0B05", outClusters: "0003,0006,0019", model:"3450-L", manufacturer: "CentraLite"
 	}
     
-    simulator {
-   		status "button 2 pressed": "catchall: 0104 0006 02 01 0140 00 6F37 01 00 0000 01 00"
-        status "button 2 released": "catchall: 0104 0006 02 01 0140 00 6F37 01 00 0000 00 00"
-    }
-    
     preferences{
-    	input ("holdTime", "number", title: "Minimum time in seconds for a press to count as \"held\"",
+    	section {
+    		input ("holdTime", "number", title: "Minimum time in seconds for a press to count as \"held\"",
         		defaultValue: 3, displayDuringSetup: false)
+        }
+        section {
+            input "checkInterval", "enum", title: "Presence timeout (minutes)",
+                defaultValue:"2", options: ["2", "3", "5"], displayDuringSetup: false
+        }
     }
 
 	tiles(scale: 2) {
+    	standardTile("presence", "device.presence", width: 4, height: 4, canChangeBackground: true) {
+            state "present", label: "Present", labelIcon:"st.presence.tile.present", backgroundColor:"#53a7c0"
+            state "not present", labelIcon:"st.presence.tile.not-present", backgroundColor:"#ffffff"
+        }
     	standardTile("button", "device.button", decoration: "flat", width: 2, height: 2) {
-        	state "default", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff"//, action: "test()"
+        	state "default", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff", action: "testCmd"
         }
 		valueTile("battery", "device.battery", decoration: "flat", width: 2, height: 2) {
 			state "battery", label:'${currentValue}% battery', unit:""
 		}
 
-		main (["battery"])
-		details(["button","battery"])
+		main (["presence","battery"])
+		details(["presence","button","battery"])
 	}
 }
 
 def parse(String description) {
-	//log.debug "Parsing '${description}'"
     def descMap = zigbee.parseDescriptionAsMap(description)
     //log.debug descMap
+    state.lastCheckin = now()
+    log.debug "lastCheckin = ${state.lastCheckin}"
+    handlePresenceEvent(true)
     
 	def results = []
     if (description?.startsWith('catchall:'))
 		results = parseCatchAllMessage(descMap)
 	else if (description?.startsWith('read attr -'))
 		results = parseReportAttributeMessage(descMap)
+    else log.trace descMap
         
 	return results;
 }
 
+def updated() {
+	startTimer()
+    configure()
+}
+
 def configure(){
+	log.debug "Configuring Smart Fob..."
 	[
 	"zdo bind 0x${device.deviceNetworkId} 1 1 6 {${device.zigbeeId}} {}", "delay 200",
     "zdo bind 0x${device.deviceNetworkId} 2 1 6 {${device.zigbeeId}} {}", "delay 200",
     "zdo bind 0x${device.deviceNetworkId} 3 1 6 {${device.zigbeeId}} {}", "delay 200",
     "zdo bind 0x${device.deviceNetworkId} 4 1 6 {${device.zigbeeId}} {}", "delay 200",
-    "zdo bind 0x${device.deviceNetworkId} 1 1 3 {${device.zigbeeId}} {}", "delay 200", //just a test. remove before publishing
-    
-    "zcl global send-me-a-report 1 0x20 0x20 3600 86400 {01}", "delay 100", //battery report request
-	"send 0x${device.deviceNetworkId} 1 1", "delay 200",
-        
-    "st rattr 0x${device.deviceNetworkId} 1 1 0x20"
-    ]
+    "zdo bind 0x${device.deviceNetworkId} 1 1 1 {${device.zigbeeId}} {}", "delay 200",
+    "zdo bind 0x${device.deviceNetworkId} 1 1 0x20 {${device.zigbeeId}} {}", "delay 200"
+    ] +
+    zigbee.configureReporting(0x0001,0x0020,0x20,20,20,0x01) +
+    zigbee.writeAttribute(0x0020,0x0000,0x23,0xF0)
 }
 
 def parseCatchAllMessage(descMap) {
 	//log.debug descMap
     if (descMap?.clusterId == "0006" && descMap?.command == "01") 		//button pressed
-    	createPressEvent(descMap.sourceEndpoint as int)
+    	handleButtonPress(descMap.sourceEndpoint as int)
     else if (descMap?.clusterId == "0006" && descMap?.command == "00") 	//button released
-    	createButtonEvent(descMap.sourceEndpoint as int)
+    	handleButtonRelease(descMap.sourceEndpoint as int)
+    else if (descMap?.clusterId == "0020" && descMap?.command == "00") 	//poll control check-in
+    	zigbee.command(0x0020, 0x00, "00", "0000")
+    else log.trace "Parse: Unhandled message: ${descMap}"
 }
 
 def parseReportAttributeMessage(descMap) {
 	if (descMap?.cluster == "0001" && descMap?.attrId == "0020") createBatteryEvent(getBatteryLevel(descMap.value))
+    else log.debug descMap
 }
 
 private createBatteryEvent(percent) {
@@ -97,43 +113,42 @@ private createBatteryEvent(percent) {
 }
 
 //this method determines if a press should count as a push or a hold and returns the relevant event type
-private createButtonEvent(button) {
-	def currentTime = now()
-    def startOfPress = device.latestState('lastPress').date.getTime()
+private handleButtonRelease(button) {
+	log.debug "lastPress state variable: ${state.lastPress}"
+    def sequenceError = {log.error "Uh oh...missed a message? Dropping this event."; state.lastPress = null; return []}	
+    
+    if (!state.lastPress) return sequenceError()
+	else if (state.lastPress.button != button) return sequenceError()
+    
+    def currentTime = now()
+    def startOfPress = state.lastPress?.time
     def timeDif = currentTime - startOfPress
     def holdTimeMillisec = (settings.holdTime?:3).toInteger() * 1000
     
+    state.lastPress = null	//we're done with this. clear it to make error conditions easier to catch
+    
     if (timeDif < 0) 
-    	return []	//likely a message sequence issue. Drop this press and wait for another. Probably won't happen...
-    else if (timeDif < holdTimeMillisec) 
-    	return createButtonPushedEvent(button)
+    //likely a message sequence issue or dropped packet. Drop this press and wait for another.
+    	return sequenceError()
+    else if (timeDif < holdTimeMillisec)
+    	return createButtonEvent(button,"pushed")
     else 
-    	return createButtonHeldEvent(button)
+    	return createButtonEvent(button,"held")
 }
 
-private createPressEvent(button) {
-	return createEvent([name: 'lastPress', value: now(), data:[buttonNumber: button], displayed: false])
+private handleButtonPress(button) {
+	state.lastPress = [button: button, time: now()]
 }
 
-private createButtonPushedEvent(button) {
-	log.debug "Button ${button} pushed"
+private createButtonEvent(button,action) {
+	log.debug "Button ${button} ${action}"
 	return createEvent([
     	name: "button",
-        value: "pushed", 
+        value: action, 
         data:[buttonNumber: button], 
-        descriptionText: "${device.displayName} button ${button} was pushed",
+        descriptionText: "${device.displayName} button ${button} was ${action}",
         isStateChange: true, 
         displayed: true])
-}
-
-private createButtonHeldEvent(button) {
-	log.debug "Button ${button} held"
-	return createEvent([
-    	name: "button",
-        value: "held", 
-        data:[buttonNumber: button], 
-        descriptionText: "${device.displayName} button ${button} was held",
-        isStateChange: true])
 }
 
 private getBatteryLevel(rawValue) {
@@ -144,8 +159,47 @@ private getBatteryLevel(rawValue) {
     return ((vBatt - min) / (max - min) * 100) as int
 }
 
+private handlePresenceEvent(present) {
+    def wasPresent = device.currentState("presence")?.value == "present"
+    if (!wasPresent && present) {
+        log.debug "Sensor is present"
+        startTimer()
+    } else if (!present) {
+        log.debug "Sensor is not present"
+        stopTimer()
+    }
+    def linkText = getLinkText(device)
+    def eventMap = [
+        name: "presence",
+        value: present ? "present" : "not present",
+        linkText: linkText,
+        descriptionText: "${linkText} has ${present ? 'arrived' : 'left'}",
+    ]
+    log.debug "Creating presence event: ${eventMap}"
+    sendEvent(eventMap)
+}
+
+private startTimer() {
+    log.debug "Scheduling periodic timer"
+    schedule("0 * * * * ?", checkPresenceCallback)
+}
+
+private stopTimer() {
+    log.debug "Stopping periodic timer"
+    unschedule()
+}
+
+def checkPresenceCallback() {
+    def timeSinceLastCheckin = (now() - state.lastCheckin) / 1000
+    def theCheckInterval = (checkInterval ? checkInterval as int : 2) * 60
+    log.debug "Sensor checked in ${timeSinceLastCheckin} seconds ago"
+    if (timeSinceLastCheckin >= theCheckInterval) {
+        handlePresenceEvent(false)
+    }
+}
+
 // handle commands
-def test() {
+def testCmd() {
     log.debug "Test"
-	zigbee.refreshData("0","4") + zigbee.refreshData("0","5") + zigbee.refreshData("1","0x0020")
+	zigbee.refreshData("0","4") + zigbee.refreshData("0","5") + zigbee.refreshData("0x20","0x0000")
 }
